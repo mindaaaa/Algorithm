9b4ee64f08c37c4242f216893cd7bb187b5f77c9
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-05-10 11:19:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,6 +1,15 @@
 function solution(n) {
-  const ternary = n.toString(3);
-  const reverseInt = ternary.split('').reverse().join('');
+  const ternary = [];
+  while (n > 0) {
+    ternary.push(n % 3);
+    n = Math.floor(n / 3);
+  }
 
-  return parseInt(reverseInt, 3);
+  // 10 진법 저장
+  let result = 0;
+  for (let i = 0; i < ternary.length; i++) {
+    result += ternary[i] * 3 ** (ternary.length - 1 - i);
+  }
+
+  return result;
 }
