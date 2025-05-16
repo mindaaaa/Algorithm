28b749908a3cfea5e69d977743f9ad2a5173b1d5
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-05-16 14:56:44 +0900

    [level 1] Title: 소수 찾기, Time: 55.41 ms, Memory: 44.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  const isPrime = Array(n + 1).fill(true);
+
+  isPrime[0] = false;
+  isPrime[1] = false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+  return isPrime.filter(Boolean).length;
+    // sol. 에라토스테네스의 체
+}
