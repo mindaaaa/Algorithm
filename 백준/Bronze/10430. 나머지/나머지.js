5de7734d05b742b513d92f31aa411758be0b6051
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-08-18 17:58:57 +0900

    [Bronze V] Title: 나머지, Time: 96 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const [a, b, c] = input.map(Number);
+console.log(`${(a+b)%c}
+${((a%c)+(b%c))%c}
+${(a*b)%c}
+${((a%c)*(b%c))%c}`);
