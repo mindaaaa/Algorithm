827e5d845f1a9f88e6a1b27612b4e7dc93248574
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2026-06-15 21:50:35 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,13 @@
 function solution(brown, yellow) {
-  const total = brown + yellow;
-  const arr = [];
-
-  for (let row = 3; row <= total; row++) {
-    if (total % row !== 0) continue;
-    const col = total / row;
-    if (row < col) continue;
-
-    if ((row - 2) * (col - 2) === yellow) {
-      return [row, col];
+    const total = brown + yellow;
+    const isWideEnough = (row, col) => row >= col;
+    const checkInnerArea = (row, col) => (row - 2) * (col - 2) === yellow;
+    const matchTile = (row, col) => isWideEnough(row, col) && checkInnerArea(row, col);
+    
+    for(let col = 3; col <= Math.sqrt(total); col++){
+        if(total % col !== 0) continue;
+        const row = total / col;
+        
+        if(matchTile(row, col)) return [row, col];
     }
-  }
 }
