58de07d4337e1bc16b75f2248e2a3a6c47c903bf
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2026-06-18 05:18:02 +0900

    [level 2] Title: 타겟 넘버, Time: 7.01 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,20 @@
 function solution(numbers, target) {
-  let sum = 0;
-  let count = 0;
+    let count = 0;
+    let sum = 0;
+    let depth = 0;
+    
+    dfs = (depth, sum) => {
+        if(depth === numbers.length){
+            if(sum === target) count += 1;
+            return;
+        }
+        
+        dfs(depth + 1, sum + numbers[depth]);
+        dfs(depth + 1, sum - numbers[depth]);
 
-  function dfs(depth, sum) {
-    if (depth === numbers.length) {
-      if (sum === target) {
-        count++;
-      }
-      return;
     }
+    
+    dfs(0, sum);
 
-    dfs(depth + 1, sum + numbers[depth]);
-    dfs(depth + 1, sum - numbers[depth]);
-  }
-
-  dfs(0, sum);
-  return count;
+    return count;
 }
